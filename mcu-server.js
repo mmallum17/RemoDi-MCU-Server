3a0719e657da2b9762f3a54279784e08a4185633
Marcus <marcusmallum@gmail.com>
2018-04-04 22:46:35 -0500

    Received Load Cell Data and Inserted into Database

--- mcu-server.js
+++ mcu-server.js
@@ -20,27 +20,33 @@ const server = net.createServer((client) => {
        //client.end();
         socket.close();
     });
-    socket.on('name', function(data) {
+    /*socket.on('name', function(data) {
         console.log(data);
-    });
+    });*/
 
     // MCU Connection Events
     client.on('data', function (chunk) {
-        console.log(chunk.toString());
+        //console.log(chunk.toString());
+        let tokens = chunk.toString().split(",");
+        let loadCellZero = parseFloat(tokens[1]);
+        let loadCellOne = parseFloat(tokens[2]);
+        let loadCellTwo = parseFloat(tokens[3]);
+        let loadCellThree = parseFloat(tokens[4]);
+        console.log(tokens);
         MongoClient.connect(uri, function(err, client) {
             if (err) {
                 throw err;
             }
             console.log("Database connected");
             const db = client.db(dbName);
-            let myobj = {date: new Date(), LC0: 0.1, LC1: 1.1, LC2: 2.1, LC3: 3.};
-            db.collection('load_cell_data').insertOne(myobj, function (err, result) {
+            let myobj = {date: new Date(), loadCellZero: loadCellZero, loadCellOne: loadCellOne, loadCellTwo: loadCellTwo, loadCellThree: loadCellThree};
+            db.collection('loadcells').insertOne(myobj, function (err, result) {
                 if (err) {
                     throw err;
                 }
                 console.log('Load Cell Data inserted');
-                client.close();
             });
+            client.close();
         });
         //socket.emit('weight', chunk.toString());
     });
